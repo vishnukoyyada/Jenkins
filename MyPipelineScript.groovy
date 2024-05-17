69aaefa83d8dcb494c4df71849be6880a0262de8
vishnukoyyad03 <vishnukoyyad03@gmail.com>
2024-05-17 11:21:49 +0530

    modified .grooovy file

--- MyPipelineScript.groovy
+++ MyPipelineScript.groovy
@@ -1,6 +1,6 @@
 pipeline {
-    agent any {
-        stages {
+    agent any 
+    stages {
             stage ("build"){
                 steps {
                     echo "build"
@@ -16,6 +16,5 @@ pipeline {
                     echo "deploy"
                 }
             }
-        }
     }
 }
